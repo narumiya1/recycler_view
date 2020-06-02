9bc8dfa5373f55c0cc1f5762f77d6cd13da4ffd5
narumiya1 <narumiiyamei@gmail.com>
2020-06-02 10:09:37 +0700

    recyclerview menggunakan cardview di android studio

--- app/src/main/java/com/example/recycler_view/MainActivity.java
+++ app/src/main/java/com/example/recycler_view/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.example.recycler_view.adapter.CardViewCoachAdapter;
 import com.example.recycler_view.adapter.ListCoachAdapter;
 import com.example.recycler_view.data.CoachData;
 import com.example.recycler_view.model.Coach;
@@ -15,8 +16,11 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private RecyclerView rvCoach ;
     private ArrayList<Coach> list = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,4 +52,12 @@ public class MainActivity extends AppCompatActivity {
     private void showSelectedCoach(Coach data) {
         Toast.makeText(this, "you choose " +data.getName(), Toast.LENGTH_SHORT).show();
     }
+
+    private void showRecyclerCardView(){
+        rvCoach.setLayoutManager(new LinearLayoutManager(this));
+        CardViewCoachAdapter cardViewCoachAdapter = new CardViewCoachAdapter(list);
+        rvCoach.setAdapter(cardViewCoachAdapter);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/recycler_view/adapter/CardViewCoachAdapter.java
@@ -0,0 +1,94 @@
+package com.example.recycler_view.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.recycler_view.R;
+import com.example.recycler_view.model.Coach;
+
+import java.util.ArrayList;
+
+public class CardViewCoachAdapter extends RecyclerView.Adapter<CardViewCoachAdapter.CardViewVolder> {
+
+    private ArrayList<Coach> listOfCoach ;
+
+    public CardViewCoachAdapter(ArrayList<Coach> list){
+        this.listOfCoach = list ;
+    }
+
+    @NonNull
+    @Override
+    public CardViewVolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_cardview, parent, false);
+        return new CardViewVolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final CardViewVolder holder, int position) {
+
+        Coach coach = listOfCoach.get(position);
+        Glide.with(holder.itemView.getContext())
+                .load(coach.getPhoto())
+                .apply(new RequestOptions().override(350,550))
+                .into(holder.imgPhoto);
+        holder.tvName.setText(coach.getName());
+        holder.tvFrom.setText(coach.getFrom());
+
+        holder.btnFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(holder.itemView.getContext(), "favourite" + listOfCoach.get(holder.getAdapterPosition()).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(holder.itemView.getContext(), "share" + listOfCoach.get(holder.getAdapterPosition()).getName(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(holder.itemView.getContext(), "kamu memilih "+listOfCoach.get(holder.getAdapterPosition()).getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listOfCoach.size();
+    }
+
+    public class CardViewVolder extends RecyclerView.ViewHolder {
+
+        ImageView imgPhoto ;
+        TextView tvName , tvFrom ;
+        Button btnFav, btnShare ;
+
+        public CardViewVolder(View itemView) {
+            super(itemView);
+            imgPhoto = itemView.findViewById(R.id.img_item_photo);
+            tvName = itemView.findViewById(R.id.tv_item_name);
+            tvFrom = itemView.findViewById(R.id.tv_item_from);
+            btnFav = itemView.findViewById(R.id.btn_set_favourite);
+            btnShare = itemView.findViewById(R.id.btn_share);
+
+
+
+        }
+    }
+}
